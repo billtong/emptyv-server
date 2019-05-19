7f7d5231511378978b7e310302bd9b2a54850879
billtong <zhiyuantongbill@gmail.com>
2019-05-19 17:56:26 -0400

    Revert "fix update files"
    This reverts commit 5b226cb9e260fcb2e91fcb623961839fe885a56f.

--- src/main/java/com/empty/controller/FileController.java
+++ src/main/java/com/empty/controller/FileController.java
@@ -6,9 +6,6 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.empty.util.DataTools;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,20 +14,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping(value = "/api/file", produces = "application/json;charset=UTF-8")
 public class FileController {
 
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
-
-
     @RequestMapping(value = "upload", method = RequestMethod.POST)
     public String uploadFile(@RequestParam String[] filepaths, MultipartFile[] files,
                              HttpServletResponse res, HttpServletRequest req) {
-        LOGGER.info(String.valueOf(files.length));
         if (files != null) {
             for (int i = 0; i < files.length; i++) {
                 String path = req.getSession().getServletContext().getRealPath("/");
-                LOGGER.info("hello!!!!");
-                LOGGER.info(path);
-
                 path = path.replace("empty-server-1.0.0", "empty-video-files").concat(filepaths[i]);
                 File desFile = new File(path);
                 if (!desFile.getParentFile().exists()) {
