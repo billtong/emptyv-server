1b0e40bdb1a52cea38239c69c2a4e5b4b57e1a8f
billtong <zhiyuantongbill@gmail.com>
2019-07-30 09:42:41 -0400

    zuul -> gateway

--- ev-api-gateway/src/main/java/com/agateram/emptyvideo/EvApiGatewayApplication.java
+++ ev-api-gateway/src/main/java/com/agateram/emptyvideo/EvApiGatewayApplication.java
@@ -3,10 +3,8 @@ package com.agateram.emptyvideo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
-@EnableZuulProxy
 @SpringBootApplication
 public class EvApiGatewayApplication {
 	public static void main(String[] args) {
